c89af4d8e057afb255ea7dcfa3a0f0dd6d89d121
hugosenari <hugosenari@gmail.com>
2022-03-22 00:31:30 -0300

    chore: re index classy

--- v0_0_3/flake.nix
+++ v0_0_3/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-classy-v0_0_3.flake = false;
   inputs.src-classy-v0_0_3.owner = "nigredo-tori";
-  inputs.src-classy-v0_0_3.ref   = "refs/tags/v0.0.3";
+  inputs.src-classy-v0_0_3.ref   = "v0_0_3";
   inputs.src-classy-v0_0_3.repo  = "classy";
   inputs.src-classy-v0_0_3.type  = "github";
   

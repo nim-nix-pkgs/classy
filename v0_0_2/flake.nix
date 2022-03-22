c335127972d1cd9e92f4e0a69518b1c95a6d30ed
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:11 -0300

    chore: re-index ../nimpkgs/c/classy/v0_0_2

--- v0_0_2/flake.nix
+++ v0_0_2/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-classy-v0_0_2.flake = false;
   inputs.src-classy-v0_0_2.owner = "nigredo-tori";
-  inputs.src-classy-v0_0_2.ref   = "refs/tags/v0.0.2";
+  inputs.src-classy-v0_0_2.ref   = "v0_0_2";
   inputs.src-classy-v0_0_2.repo  = "classy";
   inputs.src-classy-v0_0_2.type  = "github";
   
